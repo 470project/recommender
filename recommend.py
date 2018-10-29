62e8ee55d730cbe1779cedfc187ccf8358eea2c5
Karl Lawson <karllawson16@gmail.com>
2018-10-29 15:25:43 -0500

    gives a list of authors that you have not favorited but the similar users have

--- recommend.py
+++ recommend.py
@@ -7,7 +7,7 @@
 import json
 
 #user that we are matching
-matchUser = ["/u/1138361/iheartmwpp", "/u/8545331/Professor-Flourish-and-Blotts", "/u/49515/Iniga", "/u/4286546/Missbexiee", "/u/1697963/lydiamaartin", "/u/609412/Crystallic-Rain"]
+matchUser = ["/u/1138361/iheartmwpp", "/u/8545331/Professor-Flourish-and-Blotts", "/u/4286546/Missbexiee", "/u/1697963/lydiamaartin", "/u/609412/Crystallic-Rain"]
 
 #userFavs saves each users favorite authors
 userFavs = {}
@@ -37,17 +37,31 @@ for key in userFavs:
             
     jaccardDict[key] = cInter/cUnion
 
-    
+print(len(userFavs))
 
 #Sorting Jaccard Dictionary    
 sortedJaccard = sorted(jaccardDict.items(), key=lambda kv: kv[1], reverse=True)
     
 
 topTen = []
+authorsToLookAt = []
 #top ten most similar
+print("Most similar users")
 for i in range(10):
     topTen.append(sortedJaccard[i])
     print(sortedJaccard[i][0], sortedJaccard[i][1])
+    favList = userFavs[sortedJaccard[i][0]]
+    for elem in favList:
+        if elem not in matchUser:
+            authorsToLookAt.append(elem)
+
+print("")
+print("Authors to check out")
+
+for elem in set(authorsToLookAt):
+    print(elem)
+
+
 
     
 #Get top 10 users and get the stories that they rated highly and the other user has not rated.
