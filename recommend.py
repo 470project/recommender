f2d881298da0b04621c6b3d81c079e78cf67346c
Karl Lawson <karllawson16@tamu.edu>
2018-11-11 14:57:15 -0600

    Returns highest rated stories from the recommended authors
    Takes about 1 min to run.

--- recommend.py
+++ recommend.py
@@ -1,6 +1,6 @@
 '''
 * Recommender 
-* Pearson correlation for similarity and then the grab the highest ratings from the most similar users as long as those have never been rated by the target user.
+* jaccard similarity and then grab the highest ratings from the most similar users as long as those have never been rated by the target user.
 '''
 
 #read json file
@@ -11,6 +11,7 @@ matchUser = ["/u/1138361/iheartmwpp", "/u/8545331/Professor-Flourish-and-Blotts"
 
 #userFavs saves each users favorite authors
 userFavs = {}
+topStories = {}
 
 with open('result.jl') as f:
     for line in f:
@@ -21,6 +22,19 @@ with open('result.jl') as f:
             for elem in favs:
                 favAuthors.append(elem["favAuthor"])
             userFavs[j["name"]] = set(favAuthors)
+
+        if j["pageType"] == "story":
+            favs = int(j["otherInfo"]["favorites"])
+            author = j["author"]
+            link = j["storyLink"]
+            if author not in topStories:
+                topStories[author] = (link, int(favs))
+            else:
+                #if the current top story for the author has less favorites than the new story then make the new story the top story. else don't change anything.
+                if int(topStories[author][1]) < int(favs):
+                    topStories[author] = (link, int(favs))
+            
+
 
 
 #Find similar users
@@ -37,17 +51,15 @@ for key in userFavs:
             
     jaccardDict[key] = cInter/cUnion
 
-print(len(userFavs))
-
 #Sorting Jaccard Dictionary    
 sortedJaccard = sorted(jaccardDict.items(), key=lambda kv: kv[1], reverse=True)
     
 
 topTen = []
 authorsToLookAt = []
-#top ten most similar
+#top twenty most similar
 print("Most similar users")
-for i in range(10):
+for i in range(20):
     topTen.append(sortedJaccard[i])
     print(sortedJaccard[i][0], sortedJaccard[i][1])
     favList = userFavs[sortedJaccard[i][0]]
@@ -61,7 +73,10 @@ print("Authors to check out")
 for elem in set(authorsToLookAt):
     print(elem)
 
+print("")
+print("Stories to check out")
+#printing out the top stories from the authors that are recommended.
+for elem in set(authorsToLookAt):
+    if elem in topStories:
+        print(topStories[elem][0], topStories[elem][1])
 
-
-    
-#Get top 10 users and get the stories that they rated highly and the other user has not rated.
