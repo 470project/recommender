7350d208650f8977c187e6db152eca8f55d84fbf
Ryan Chomistek <ryan.chomistek@tamu.edu>
2018-11-15 18:51:20 -0600

    added class structure

--- cluster.py
+++ cluster.py
@@ -8,148 +8,155 @@ import json_lines
 import itertools
 import collections
 from collections import Counter
-
-with open('result.jl', 'rb') as f:
-    entities = [x for x in json_lines.reader(f)]
-    stories = [x for x in entities if str(x['pageType']) == "story" and str(x['storyType']) == '/book/Harry-Potter/']
-    storiesNotPotter = [x for x in entities if str(x['pageType']) == "story" and str(x['storyType']) != '/book/Harry-Potter/']
-    reviews = [x for x in entities if str(x['pageType']) == "review"]
-    users = [x for x in entities if str(x['pageType']) == "user"]
-    
-    print(len(stories),len(storiesNotPotter),len(reviews),len(users))
-
-#generate character id mappings
-characterFreqs = Counter({})
-for x in stories:
-    characterFreqs += Counter(x['characters'])
-characterFreqs = dict(characterFreqs)
-charList = list(characterFreqs.keys())
-idToChar = {charList.index(x): x for x in charList}
-charToId = {x : charList.index(x) for x in charList}
-
-def foldDict(chars):
-    charsMult = [(str(charToId[char]) + ' ') * freq for char, freq in chars.items()]
-    charsStr = ''
-    for charStr in charsMult:
-        charsStr += charStr
-    return charsStr
-
-#get all the characters present
-charList = [ foldDict(story['characters']) for story in stories]
-
-#make the lda model
-n_features = 5000
-n_topics = 100
-n_top_words = 10
-
-train = charList
-tf_vectorizer = CountVectorizer(max_df=0.8, min_df=10,
-                                max_features=n_features,
-                                stop_words='english')
-
-tf = tf_vectorizer.fit_transform(train)
-lda = LatentDirichletAllocation(n_components=n_topics, max_iter=20,
-                                learning_method='online',
-                                learning_offset=50.)
-
-ldaalloc = lda.fit(tf)
-
-tf_feature_names = tf_vectorizer.get_feature_names()
-
 import numpy as np
-def buildMatrix(model, num_topics, feature_names):
-    matrix = np.zeros([num_topics, len(feature_names)])
-    for topic_idx, topic in enumerate(model.components_):
-        matrix[topic_idx] = np.array(topic)
-    return matrix
 
-LDATopicMatrix = np.transpose(buildMatrix(lda, n_topics, tf_feature_names))
+class cluster():
+    def __init__(self):
+        with open('result.jl', 'rb') as f:
+            self.entities = [x for x in json_lines.reader(f)]
+            self.stories = [x for x in self.entities if str(x['pageType']) == "story" and str(x['storyType']) == '/book/Harry-Potter/']
+            self.storiesNotPotter = [x for x in self.entities if str(x['pageType']) == "story" and str(x['storyType']) != '/book/Harry-Potter/']
+            self.reviews = [x for x in self.entities if str(x['pageType']) == "review"]
+            self.users = [x for x in self.entities if str(x['pageType']) == "user"]
+            
+            print(len(self.stories),len(self.storiesNotPotter),len(self.reviews),len(self.users))
 
-charIdToFeatureId = {}
-featureIdToCharId = {}
+        #generate character id mappings
+        characterFreqs = Counter({})
+        for x in self.stories:
+            characterFreqs += Counter(x['characters'])
+        characterFreqs = dict(characterFreqs)
+        charList = list(characterFreqs.keys())
+        self.idToChar = {charList.index(x): x for x in charList}
+        self.charToId = {x : charList.index(x) for x in charList}
 
-featureId = 0
+        def foldDict(chars):
+            charsMult = [(str(self.charToId[char]) + ' ') * freq for char, freq in chars.items()]
+            charsStr = ''
+            for charStr in charsMult:
+                charsStr += charStr
+            return charsStr
 
-for charId in tf_feature_names:
-    #print(featureId,charId, idToChar[int(charId)])
-    charId = int(charId)
-    charIdToFeatureId[charId] = featureId
-    featureIdToCharId[featureId] = charId
-    featureId += 1
+        #get all the characters present
+        charList = [ foldDict(story['characters']) for story in self.stories]
 
-def convertCharDicToFeatureVec(story, feature_names):
-    totalCharFreq = 0
-    featureVec = np.zeros((len(feature_names)))
-    chars = dict(story['characters'])
-    for char, freq in chars.items():
-        totalCharFreq += freq
-    for char, freq in chars.items():
-        if(charToId[char] in charIdToFeatureId):
-            featureId = charIdToFeatureId[charToId[char]]
-            featureVec[featureId] = freq
-    return featureVec
+        #make the lda model
+        n_features = 5000
+        self.n_topics = 100
+        n_top_words = 10
 
-storyLinkToIdDict = {}
-IdToStoryDict = {}
-storyId = 0
-for story in stories:
-    #matrix[storyId] = convertCharDicToFeatureVec(story, feature_names)
-    storyLinkToIdDict[story['storyLink']] = storyId
-    IdToStoryDict[storyId] = story
-    storyId += 1
-    
-def buildDocCharacterMatrix(feature_names):
-    matrix = np.zeros([len(stories), len(feature_names)])
-    #print(len(feature_names), len(charToId))
-    
-    for story in stories:
-        storyId = storyLinkToIdDict[story['storyLink']]
-        matrix[storyId] = convertCharDicToFeatureVec(story, feature_names)
+        train = charList
+        tf_vectorizer = CountVectorizer(max_df=0.8, min_df=10,
+                                        max_features=n_features,
+                                        stop_words='english')
+
+        tf = tf_vectorizer.fit_transform(train)
+        lda = LatentDirichletAllocation(n_components=self.n_topics, max_iter=20,
+                                        learning_method='online',
+                                        learning_offset=50.)
+
+        ldaalloc = lda.fit(tf)
+
+        self.tf_feature_names = tf_vectorizer.get_feature_names()
         
-    #matrix = (matrix / matrix.sum())
-    return matrix;
+        #build the term to topic score matrix
+        self.LDATopicMatrix = np.zeros([self.n_topics, len(self.tf_feature_names)])
+        for topic_idx, topic in enumerate(lda.components_):
+            self.LDATopicMatrix[topic_idx] = np.array(topic)
+        self.LDATopicMatrix = self.LDATopicMatrix.transpose()
 
-docCharMatrix = buildDocCharacterMatrix(tf_feature_names)
+        self.charIdToFeatureId = {}
+        self.featureIdToCharId = {}
 
-storyTopicScores = np.matmul(docCharMatrix, LDATopicMatrix)
-predictedTopics = [storyTopicScores[i].argmax() for i in range(len(storyTopicScores))] 
+        featureId = 0
+        #make the dictionary to go from character id to feature id
+        #features have some characters left out if they are too frequent or infrequent
+        for charId in self.tf_feature_names:
+            charId = int(charId)
+            self.charIdToFeatureId[charId] = featureId
+            self.featureIdToCharId[featureId] = charId
+            featureId += 1
 
-def getTopicScoresForStory(story, TopicMatrix, featureNamesList):
-    storyFeatureVec = convertCharDicToFeatureVec(story, featureNamesList)
-    predictedTopicScores = np.zeros(len(TopicMatrix[0]))
-    
-    for i in range(len(TopicMatrix[0])):
-        topicVec = TopicMatrix[:,i]
-        predictedTopicScores[i] = np.dot(storyFeatureVec, topicVec)
+        self.storyLinkToIdDict = {}
+        self.IdToStoryDict = {}
+
+        #create a dict between storied and their id
+        storyId = 0
+        for story in self.stories:
+            self.storyLinkToIdDict[story['storyLink']] = storyId
+            self.IdToStoryDict[storyId] = story
+            storyId += 1
+            
+        def buildDocCharacterMatrix(feature_names):
+            matrix = np.zeros([len(self.stories), len(feature_names)])
+            for story in self.stories:
+                storyId = self.storyLinkToIdDict[story['storyLink']]
+                matrix[storyId] = self.convertCharDicToFeatureVec(story, feature_names)
+            return matrix
+
+        #make a matrix that holds the character frequencies in each story
+        docCharMatrix = buildDocCharacterMatrix(self.tf_feature_names)
+
+        self.storyTopicScores = np.matmul(docCharMatrix, self.LDATopicMatrix)
+
+    def convertCharDicToFeatureVec(self, story, feature_names):
+            totalCharFreq = 0
+            featureVec = np.zeros((len(feature_names)))
+            chars = dict(story['characters'])
+            for char, freq in chars.items():
+                totalCharFreq += freq
+            for char, freq in chars.items():
+                if(self.charToId[char] in self.charIdToFeatureId):
+                    featureId = self.charIdToFeatureId[self.charToId[char]]
+                    featureVec[featureId] = freq
+            return featureVec
+
+    def getTopicScoresForStory(self, story, TopicMatrix, featureNamesList):
+        storyFeatureVec = self.convertCharDicToFeatureVec(story, featureNamesList)
+        predictedTopicScores = np.zeros(len(TopicMatrix[0]))
         
-    return predictedTopicScores
+        for i in range(len(TopicMatrix[0])):
+            topicVec = TopicMatrix[:,i]
+            predictedTopicScores[i] = np.dot(storyFeatureVec, topicVec)
+            
+        return predictedTopicScores
 
-def magnitude(vec):
-    return np.linalg.norm(vec)
+    def getScoresForUser(self, user):
+        def magnitude(vec):
+            return np.linalg.norm(vec)
 
-def cosine(storyVec, TopicVec):
-    ANorm = magnitude(storyVec)
-    BNorm = magnitude(TopicVec)
-    dot = np.dot(storyVec, TopicVec)
-    if((ANorm*BNorm) == 0):
-        return 0
-    res = (dot / (ANorm*BNorm))
-    return res
+        def cosine(storyVec, TopicVec):
+            ANorm = magnitude(storyVec)
+            BNorm = magnitude(TopicVec)
+            dot = np.dot(storyVec, TopicVec)
+            if((ANorm*BNorm) == 0):
+                return 0
+            res = (dot / (ANorm*BNorm))
+            return res
+        
+        #get the users favorite stories
+        storyIds = []
+        for favorite in user['favorites']:
+            if(favorite['favStory'] in self.storyLinkToIdDict):
+                storyIds.append(self.storyLinkToIdDict[favorite['favStory']])
+        topicScores = np.zeros(self.n_topics)
+        #get the topic scores for each story
+        for id in storyIds:
+            topicScores += self.getTopicScoresForStory(self.IdToStoryDict[id], self.LDATopicMatrix, self.tf_feature_names)
+        topicScores = topicScores / magnitude(topicScores)
 
-def getScoresForUser(user):
-    storyIds = []
-    for favorite in user['favorites']:
-        if(favorite['favStory'] in storyLinkToIdDict):
-            storyIds.append(storyLinkToIdDict[favorite['favStory']])
-    topicScores = np.zeros(n_topics)
-    for id in storyIds:
-        topicScores += getTopicScoresForStory(IdToStoryDict[id], LDATopicMatrix, tf_feature_names)
-    print(len(LDATopicMatrix), len(LDATopicMatrix[0]))
-    topicScores = topicScores / magnitude(topicScores)
-    storySimilarities = np.zeros(len(storyTopicScores))
-    for i in range(len(storyTopicScores)):
-        otherStoryTopicScores = storyTopicScores[i]
-        storySimilarities[i] = cosine(topicScores, otherStoryTopicScores)
-    return storySimilarities
-    
-print(getScoresForUser(users[0]))
+        #find all story sim scores
+        storySimilarities = np.zeros(len(self.storyTopicScores))
+        for i in range(len(self.storyTopicScores)):
+            otherStoryTopicScores = self.storyTopicScores[i]
+            storySimilarities[i] = cosine(topicScores, otherStoryTopicScores)
+        return storySimilarities
+            
+#print(getScoresForUser(self.users[0]))
+clusterRecommender = cluster()
+import time
+
+start = time.time()
+print(clusterRecommender.getScoresForUser(clusterRecommender.users[0]))
+print(time.time() - start)
+
