6a71c0c4421547709405af985d4730c2940f7311
ryan-chomistek <ryan.chomistek@tamu.edu>
2018-11-19 18:54:05 -0600

    updated pR

--- pageRankRecommender.py
+++ pageRankRecommender.py
@@ -27,27 +27,44 @@ class pageRankRecommender():
             storyLinkToIdDict[story['storyLink']] = lastStoryId
             IdToStoryDict[lastStoryId] = story
             lastStoryId += 1
-
+        
         self.storyLinkToScores = {}
+
+        minScore = 0
+        maxScore = 0
 
         for res in PRresults:
             score = res['score']
+            minScore = min(score, minScore)
+            maxScore = max(score, maxScore)
+        print(minScore, maxScore)
+        delta = maxScore - minScore
+        scale = 1 / delta 
+
+        for res in PRresults:
+            score = res['score'] / delta
             link = res['link']
 
             if(link in userLinkToIdDict):
                 user = IdToUserDict[userLinkToIdDict[link]]
                 for story in user['stories']:
-                    self.storyLinkToScores[story] = score
+                    if(story in storyLinkToIdDict):
+                        self.storyLinkToScores[story] = score
 
     def predict(self):
         return self.storyLinkToScores
 
-'''
+
+#%%
+PRRec = pageRankRecommender(stories, users)
+scores = [ (link, score)for link, score in PRRec.predict().items()]
+
+#print(scores[:10])
+print(sorted(scores, key=lambda tup: tup[1],reverse=True)[:10])
+
+#%%
+
 with open('result.jl', 'rb') as f:
     entities = [x for x in json_lines.reader(f)]
     stories = [x for x in entities if str(x['pageType']) == "story" and str(x['storyType']) == '/book/Harry-Potter/']
     users = [x for x in entities if str(x['pageType']) == "user"]
-
-PRRec = pageRankRecommender(stories, users)
-print(len(PRRec.predict()))
-'''
